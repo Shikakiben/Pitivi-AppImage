9ec297e4d39985ca58852dabd08b054e92f4f410
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2026-03-28 18:50:46 +0100

    Comment out PATH_MAPPING export in make-appimage.sh
    Comment out the PATH_MAPPING export in the script.

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,7 @@ export DESKTOP=/usr/share/applications/org.pitivi.Pitivi.desktop
 export DEPLOY_PYTHON=1
 export DEPLOY_GTK=1
 export DEPLOY_GDK=1
-export PATH_MAPPING='
+#export PATH_MAPPING='
   /usr/lib/pitivi:\${SHARUN_DIR}/lib/pitivi
   /usr/share/pitivi:\${SHARUN_DIR}/share/pitivi
 '
